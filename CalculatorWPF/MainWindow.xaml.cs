33ef64b3b89089c82a6d29601d4a42528d509106
The Real <sashasan2003@mail.ru>
2022-10-23 02:00:10 +0300

    Added logic for number buttons and some another buttons

--- CalculatorWPF/MainWindow.xaml.cs
+++ CalculatorWPF/MainWindow.xaml.cs
@@ -20,13 +20,18 @@ namespace CalculatorWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            string Text1 = "", Text2 = "";
+            bool Second_Lay = false;
         }
 
 
-
+        private string Text1 = "", Text2 = "";
+        private bool Second_Lay = false;
+        char Operat = '0';
         private DateTime click_Started;
 
         private void Button_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -36,19 +41,67 @@ namespace CalculatorWPF
 
         private void Button_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
+            if (Text.Text.Length > 10 || (Text.Text.Length > 11 && Text.Text.Contains("."))) {
+                Text.Text = "E";
+                return;
+            }
+            if (Text.Text == "E")
+            {
+                Text.Text = "";
+            }
             if ((DateTime.Now - click_Started).TotalSeconds > 0.2)
             {
-                Text.Text += '.';
+                if (!Text.Text.Contains(".") && Text.Text != "")
+                {
+                    Text.Text += ".";
+                }
             }
             else
             {
-                Text.Text += '0';
+                if (Text.Text != "0")
+                {
+                    Text.Text += "0";
+                }
             }
         }
 
-        private void B0_Click(object sender, RoutedEventArgs e)
+        private void Button_number_Click(object sender, RoutedEventArgs e)
         {
+            if (Text.Text.Length > 10 || (Text.Text.Length > 11 && Text.Text.Contains(".")))
+            {
+                Text.Text = "E";
+                return;
+            }
+            if (Text.Text == "E")
+            {
+                Text.Text = "";
+            }
+            var btn = (Button)e.Source;
+            Text.Text += btn.Content;
 
         }
+
+        private void B_result_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = e.Source as Button;
+        }
+
+        private void B_minus_Click(object sender, RoutedEventArgs e)
+        {
+            if (Text.Text == "" || Text.Text == "E")
+            {
+                Text.Text += "-";
+            } else
+            {
+                if (!Second_Lay)
+                {
+                    Text1 = Text.Text;
+                    Text.Text = "";
+                    Operat = '-';
+                }
+            }
+        }
+
+
     }
 }
